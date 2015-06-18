858d601a501174e46426d7564708aa01023cf1a2
ahmed hmed <ahmed7med@gmail.com>
2015-06-18 22:31:41 +0300

    Create login_form.php

---
+++ login_form.php
@@ -0,0 +1,7 @@
+<div class="login">
+		{{ Form::open()}}
+			<input type="email" name="email" placeholder= "الاسم الاول">
+			<input type="password" name="password" placeholder="كلمة السر">
+			<input type="submit" name="login" value="دخول">
+		{{ Form::close()}}
+	</div>
